4c5e72b370ad13c0808c74a1eb7cb9f1c8504f89
Đinh Lê Mạnh Duy <manhduydl@gmail.com>
2016-04-16 17:18:17 +0700

    add workspace activity

--- app/src/main/java/com/kun/allgo/Views/AddWorkspaceActivity.java
+++ app/src/main/java/com/kun/allgo/Views/AddWorkspaceActivity.java
@@ -1,16 +1,39 @@
 package com.kun.allgo.Views;
 
+import android.Manifest;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.model.LatLng;
+import com.kun.allgo.Global.GlobalVariable;
 import com.kun.allgo.Models.Workspace;
 import com.kun.allgo.R;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 
-public class AddWorkspaceActivity extends AppCompatActivity {
+public class AddWorkspaceActivity extends AppCompatActivity implements ConnectionCallbacks, OnConnectionFailedListener{
 
     private EditText edtWordSpaceName;
     private EditText edtWorkspaceDescription;
@@ -19,6 +42,8 @@ public class AddWorkspaceActivity extends AppCompatActivity {
     private Button btnFindPlace;
     private Button btnSubmit;
     private Button btnGetCurrentLocation;
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
 
     private Workspace workspace = new Workspace();
     @Override
@@ -28,8 +53,18 @@ public class AddWorkspaceActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // Create an instance of GoogleAPIClient.
+        buildGoogleApiClient();
+        //
+
         getFormWidget();
-        //addEvent();
+        addEvent();
+    }
+
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
     }
 
     private void getFormWidget() {
@@ -40,13 +75,31 @@ public class AddWorkspaceActivity extends AppCompatActivity {
         btnFindPlace = (Button) findViewById(R.id.btnFindPlace);
         btnSubmit = (Button) findViewById(R.id.btnSubmitWorkspace);
         btnGetCurrentLocation = (Button) findViewById(R.id.btnGetCurrentLocation);
+
+        edtLatitde.setText(GlobalVariable.latitude);
+        edtLongitude.setText(GlobalVariable.longititude);
+        GlobalVariable.latitude = "";
+        GlobalVariable.longititude = "";
     }
 
     private void addEvent() {
+
+        btnGetCurrentLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mGoogleApiClient != null) {
+                    mGoogleApiClient.connect();
+
+                }
+            }
+        });
+
         btnFindPlace.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //Gọi Activity Map
+                Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+                startActivity(intent);
             }
         });
 
@@ -54,7 +107,7 @@ public class AddWorkspaceActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //Thực hiện save Workspace
-                //saveWorkspace();
+                saveWorkspace();
             }
         });
     }
@@ -63,4 +116,108 @@ public class AddWorkspaceActivity extends AppCompatActivity {
         return true;
     }
 
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+        if (checkGPS()) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        }
+        if (mLastLocation != null) {
+
+            edtLatitde.setText(String.valueOf(mLastLocation.getLatitude()));
+            edtLongitude.setText(String.valueOf(mLastLocation.getLongitude()));
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(this, "Failed to connect...", Toast.LENGTH_SHORT).show();
+    }
+
+    //Phan Get Location
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    private boolean checkGPS()
+    {
+        LocationManager lm = (LocationManager)getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+        boolean gps_enabled = false;
+        boolean network_enabled = false;
+
+        try {
+            gps_enabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        } catch(Exception ex) {}
+
+        try {
+            network_enabled = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        } catch(Exception ex) {}
+
+        if(!gps_enabled && !network_enabled) {
+            // notify user
+            FragmentManager fm = getSupportFragmentManager();
+            MyAlertDialogFragment alertDialog = MyAlertDialogFragment.newInstance();
+            alertDialog.show(fm, "fragment_alert");
+            return false;
+        }
+        return true;
+    }
+
+    public static class MyAlertDialogFragment extends DialogFragment {
+        public MyAlertDialogFragment() {
+            // Empty constructor required for DialogFragment
+        }
+
+        public static MyAlertDialogFragment newInstance() {
+            MyAlertDialogFragment frag = new MyAlertDialogFragment();
+            Bundle args = new Bundle();
+//            args.putString("title", title);
+            frag.setArguments(args);
+            return frag;
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            AlertDialog.Builder dialog = new AlertDialog.Builder(getActivity());
+            dialog.setMessage("Để xác định vị trí hiện tại, bạn phải mở dịch vụ GPS của thiết bị.");
+            dialog.setPositiveButton("Setting", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                    // TODO Auto-generated method stub
+                    Intent myIntent = new Intent( Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    getActivity().startActivity(myIntent);
+                    //get gps
+                }
+            });
+            dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                    // TODO Auto-generated method stub
+
+                }
+            });
+
+            return dialog.create();
+        }
+    }
 }
